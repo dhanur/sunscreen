11f555ce979d08f7b670a999ed62c8b10cdab181
Dhanur Grandhi <dhanurg@gmail.com>
2013-11-24 16:28:14 -0800

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sunscreen::Application.config.secret_key_base = 'd2484657652373ea5d6d4694e3631382cf805594c07976b387ab50a32fba3c58aca0decdaa7ebd62b6fd765df8c693e1fc4a61b7adb7c12f532174e30136e2c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sunscreen::Application.config.session_store :cookie_store, key: '_sunscreen_session'
